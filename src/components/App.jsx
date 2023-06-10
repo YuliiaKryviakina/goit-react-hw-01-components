eb5845aa26943bc9aabeebe9e675a6a93ed60a1a
YuliiaKryviakina <117831702+YuliiaKryviakina@users.noreply.github.com>
2023-06-10 15:47:21 +0300

    task02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import user from "data/user";
+import data from "data/data";
 import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 };
